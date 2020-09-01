059a52b03d6673ae621b34c9d6bdaf0bcb078154
Lukas Sinkar <lukassinkarofficial@gmail.com>
2020-09-01 17:21:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [spin, setSpin] = useState(false);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div onClick={() => setSpin(!spin)}>
+                    <img
+                        src={logo}
+                        className="App-logo"
+                        alt="logo"
+                        style={{ animation: spin ? "none" : "App-logo-spin infinite 20s linear" }}
+                    />
+                </div>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
